86701e8665f2fceef08705a7f908fee3b59cc3f6
Melissa Solórzano <163376726+kmmelissat@users.noreply.github.com>
2026-02-23 19:01:55 -0600

    🗑️ chore: clean up font definitions by removing unused font styles and improving formatting

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -8,23 +8,17 @@ export const menaGrotesk = localFont({
       weight: "400",
       style: "normal",
     },
-    { path: "../fonts/Mena Grotesk Medium.otf", weight: "500", style: "normal" },
+    {
+      path: "../fonts/Mena Grotesk Medium.otf",
+      weight: "500",
+      style: "normal",
+    },
     {
       path: "../fonts/Mena Grotesk SemiBold.otf",
       weight: "600",
       style: "normal",
     },
     { path: "../fonts/Mena Grotesk Bold.otf", weight: "700", style: "normal" },
-    {
-      path: "../fonts/Mena Grotesk Regular Italic.otf",
-      weight: "400",
-      style: "italic",
-    },
-    {
-      path: "./fonts/Mena Grotesk Bold Italic.otf",
-      weight: "700",
-      style: "italic",
-    },
   ],
   variable: "--font-mena-grotesk",
 });
